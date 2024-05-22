6ad0df0bb76a080fd69877c19248a0314fb947f0
Ahsanbati <155074537+Ahsanbati@users.noreply.github.com>
2024-05-22 17:53:16 +0000

    container config done

---
+++ _01Basic/01datatypes.js
@@ -0,0 +1,6 @@
+//There are two types of data Primitive and non_Primitive/Reference data types
+//Primitive data types has 7 types.String,Number,Boolean,Null,Undefind,Symbol,BigInt
+
+//Non_Primitive/Reference 
+//Array,Objeccts,Functions
+//there are two way to push data 
---
+++ _01Basic/index.js
@@ -0,0 +1,6 @@
+console.log("AhsanBati")
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
